1ed0d6952fdf7cf92c2c88af82b2ead0d0c4cff0
RomanDorosh <roma.dorosh@gmail.com>
2021-11-09 22:54:32 +0100

    Starting the Store

---
+++ src/app/model/model.module.ts
@@ -0,0 +1,9 @@
+import { StaticDataSource } from './static.datasource';
+import { ProductRepository } from './product.repository';
+import { NgModule } from "@angular/core";
+
+
+@NgModule({
+    providers: [ProductRepository, StaticDataSource]
+})
+export class ModelModule { }
