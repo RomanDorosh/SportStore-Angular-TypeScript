4e9681764eae5f342464b1ce07f6d546c40df274
RomanDorosh <roma.dorosh@gmail.com>
2021-11-12 18:54:06 +0100

    Adding URL Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,32 @@
+import { StoreFirstGuard } from './storeFirst.guard';
+import { StoreComponent } from './store/store.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, StoreModule],
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      {
+        path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path: "**", redirectTo: "/store" }
+    ])],
+  providers: [StoreFirstGuard],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,3 +1,5 @@
+import { CheckoutComponent } from './checkout.component';
+import { CartDetailComponent } from './cartDetail.component';
 import { CartSummaryComponent } from './cartSummary';
 import { StoreComponent } from './store.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,10 +7,12 @@ import { ModelModule } from './../model/model.module';
 import { NgModule } from "@angular/core";
 import { FormsModule } from '@angular/forms';
 import { CounterDirective } from './counter.directive';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
-    exports: [StoreComponent]
+    imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent,
+        CartDetailComponent, CheckoutComponent],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 export class StoreModule { }
