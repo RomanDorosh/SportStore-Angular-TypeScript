03335ab5eda98ac1b2486dc525c942a97d6c527e
RomanDorosh <roma.dorosh@gmail.com>
2021-11-20 16:49:47 +0100

    Preparing Common Content and the Feature Module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,3 +1,6 @@
+import { OrderTableComponent } from './orderTable.component';
+import { ProductTablerComponent } from './productTable.component';
+import { ProductEditorComponent } from './productEditor.component';
 import { AdminComponent } from './admin.component';
 import { RouterModule } from "@angular/router";
 import { AuthComponent } from "./auth.component";
@@ -8,13 +11,23 @@ import { AuthGuard } from "./auth.guard";
 
 let routing = RouterModule.forChild([
     { path: "auth", component: AuthComponent },
-    { path: "main", component: AdminComponent, canActivate: [AuthGuard] },
+    {
+        path: "main", component: AdminComponent, canActivate: [AuthGuard],
+        children: [
+            { path: "products/:mode/:id", component: ProductEditorComponent },
+            { path: "products/:mode", component: ProductEditorComponent },
+            { path: "products", component: ProductTablerComponent },
+            { path: "orders", component: OrderTableComponent },
+            { path: "**", redirectTo: "products" }
+        ]
+    },
     { path: "**", redirectTo: "auth" }
 ]);
 
 @NgModule({
     imports: [CommonModule, FormsModule, routing],
     providers: [AuthGuard],
-    declarations: [AuthComponent, AdminComponent]
+    declarations: [AuthComponent, AdminComponent,
+        ProductTablerComponent, ProductEditorComponent, OrderTableComponent]
 })
 export class AdminModule { }
