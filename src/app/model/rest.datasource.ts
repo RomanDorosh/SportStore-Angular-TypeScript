6ee6691ea69a176046c90f9985bc01fec367502c
RomanDorosh <roma.dorosh@gmail.com>
2021-11-20 17:50:16 +0100

    Implementing Orders Feature and finishing with Administration

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -12,14 +12,18 @@ const PORT = 3500;
 
 @Injectable()
 export class RestDataSource {
+
     baseUrl: string;
     auth_token: string;
+
     constructor(private http: HttpClient) {
         this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
     }
+
     getProducts(): Observable<Product[]> {
         return this.http.get<Product[]>(this.baseUrl + "products");
     }
+
     saveOrder(order: Order): Observable<Order> {
         return this.http.post<Order>(this.baseUrl + "orders", order);
     }
